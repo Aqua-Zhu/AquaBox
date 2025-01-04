d27206ba13ee09e6985472b7d4b518fc7e871aa8
Aqua-Zhu <z891202z@gmail.com>
2025-01-05 01:29:48 +0800

    0104

--- BioMonsterVault/src/pages/Home.jsx
+++ BioMonsterVault/src/pages/Home.jsx
@@ -1,13 +1,13 @@
 import { Link } from "react-router-dom";
-
 export default function Home() {
 
 
 
     return (
         <>
+            {/* 頁首區 */}
             <div id="topbar">
-                <h1><img src="" alt="" />BioMonsterVault</h1>
+                <h1><Link to='/'><img src="" alt="" />Biomonster Vault</Link></h1>
                 <nav>
                     <ul>
                         <li><Link>HIT</Link></li>
@@ -17,12 +17,37 @@ export default function Home() {
                     </ul>
                 </nav>
             </div>
+            {/* Banner區 */}
             <header id="banner">
-                <img src="./images/banner-border.svg" alt="" />
-                <button id="bannerBtn">post</button>
+                <img src="./images/banner-border.png" alt="" />
+                <h2>Biomonster Vault</h2>
+                <button id="bannerBtn">
+                    <span id="bannerBtnBlock1"></span>
+                    <span id="bannerBtnBlock2"></span>
+                    <p id="bannerBtnText">POST &gt;</p>
+                </button>
             </header>
             <main>
+                {/* hit */}
                 <section id="hit">
+                    <header className="title">
+                        <figure id="hitTop">
+                            <img src="./images/TitleWrapperShort.svg" className="titleTop1"></img>
+                            <img src="./images/TitleInnerShort.svg" className="titleTop2"></img>
+                            <img src="./images/TitleChargeShort.svg" className="titleTop3"></img>
+                        </figure>
+
+                        <h2 className="h2main">
+                            HIT
+                            <h2 className="h2bottom">HIT</h2>
+                        </h2>
+
+                        <figure id="hitBottom">
+                        <img src="./images/TitleBottomLong.svg" alt="" className="titleBottom" />
+                        {/* <img src="./images/" alt="" className="titleBottom" />
+                        <img src="" alt="" className="titleBottom" /> */}
+                        </figure>
+                    </header>
                     <article className="">
                         <figure className=""></figure>
                         <div className="hit1">
@@ -55,6 +80,19 @@ export default function Home() {
                     </article>
                 </section>
                 <section id="news">
+                    <header className="title">
+                        <figure id="newsTop">
+                            <img src="./images/TitleTopShort-blue.svg" alt="" />
+                        </figure>
+                        <h2 className="h2main">
+                            NEWS
+                            <h2 className="h2bottom">NEWS</h2>
+                        </h2>
+
+                        <figure id="newsBottom">
+                        <img src="./images/TitleBottomLong-blue.svg" alt="" />
+                        </figure>
+                    </header>
                     <article id="news-1">
                         <h3>
                             <div className="newsLine1"></div>
@@ -76,9 +114,13 @@ export default function Home() {
                     <button id="moreBtn">MORE</button>
                 </section>
                 <section id="about">
-                    <h2>
-                        ABOUT
-                    </h2>
+                    <header className="title">
+                        <h2 className="h2main">
+                            ABOUT
+                            <h2 className="h2bottom">ABOUT</h2>
+                        </h2>
+
+                    </header>
                     <hr />
                     <hr />
                     <div id="aboutTitle">
